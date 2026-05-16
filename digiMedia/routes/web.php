94758b818e41dc1c634fe40dc13e1dcf582c8179
Oussamaycode <oussamalamlouki047@gmail.com>
2026-05-17 00:01:14 +0100

    Updated Changes

--- digiMedia/routes/web.php
+++ digiMedia/routes/web.php
@@ -25,3 +25,4 @@ Route::get('home',function(){
 
 Route::post('/register',[AuthController::class,'register'])->name('register');
 Route::post('/Login',[AuthController::class,'login'])->name('login');
+Route::post('/home,[]')->med
